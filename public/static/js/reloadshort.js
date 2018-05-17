7f11d40c349d4db6ccc3289ab9bc6d55c6b5ed20
袁宜照 <zzwyxl@163.com>
2018-05-17 21:31:16 +0800

    完成简答题打回的编辑

--- public/static/js/reloadshort.js
+++ public/static/js/reloadshort.js
@@ -1,28 +1,31 @@
 var childnum = 0;
 
-function send_child_number(data) {
+function send_child_number(data,belongpaper) {
     var new_score = 0;
     childnum = data;
+    swal("等待", "正在更新", "info");
     if (childnum > 0) {
-        swal("等待","正在更新","info");
         for (i = 0; i < childnum; i++) {
-           new_score+=parseInt($("#big-score").val());
+            new_score += parseInt($("#small-score-" + i).val());
         }
         $.post("/testpaper/public/index.php/api/Shortanswer/update", {
             id: $("#big-id").val(),
             name: $("#big-name").val(),
-            answer: $("#big-answer").val(),
+            answer: "",
             score: new_score
+        }).done(function () {
+            for (i = 0; i < childnum; i++) {
+                $.post("/testpaper/public/index.php/api/Shortanswer/update", {
+                    id: $("#id-" + i).val(),
+                    name: $("#small-name-" + i).val(),
+                    answer: $("#small-answer-" + i).val(),
+                    score: $("#small-score-" + i).val()
+                }).done(function () {
+                });
+            }
+            swal("完成", "修改成功", "success");
         });
-        for (i = 0; i < childnum; i++) {
-            $.post("/testpaper/public/index.php/api/Shortanswer/update", {
-                id: $("#id-" + i).val(),
-                name: $("#small-name-" + i).val(),
-                answer: $("#small-answer-" + i).val(),
-                score: $("#small-score-" + i).val()
-            }).done(function(){
-            });
-        }
+
     }
     else {
         $.post("/testpaper/public/index.php/api/Shortanswer/update", {
@@ -30,7 +33,11 @@ function send_child_number(data) {
             name: $("#big-name").val(),
             answer: $("#big-answer").val(),
             score: $("#big-score").val()
+        }).done(function(){
+            swal("完成", "修改成功", "success").then((ok)=>{
+                window.location.href = "/testpaper/public/index.php/uploader/overview/index/id/"+belongpaper;
+            });
         });
     }
-    swal("完成","修改成功","success");
+
 }
