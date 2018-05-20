df0b28a8816aa316344267a84fda3ba1a9b326e7
hanerx <zhangyu199946@gmail.com>
2018-05-20 20:28:50 +0800

    admin界面

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,19 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+use \app\api\model\User as UserModel;
+class Index extends Controller{
+    public function index(){
+        if(isset($_COOKIE['userid'])){
+            $user=new \app\api\controller\User();
+            if($user->checkuser($_COOKIE['userid'])){
+                $data=UserModel::get(["Cookie"=>$_COOKIE['userid']]);//从数据库调取此用户信息
+                if($data){
+                    $this->assign("user",$data);
+                    return $this->fetch();
+                }
+            }
+        }
+        return $this->error('请先登录','index/index/index');
+    }
+}
